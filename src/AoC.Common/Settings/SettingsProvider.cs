ad051af020694581f8c4d3613a0696a0b72bb047
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-11-30 20:20:31 +0100

    Add local appsettings for session

--- src/AoC.Common/Settings/SettingsProvider.cs
+++ src/AoC.Common/Settings/SettingsProvider.cs
@@ -8,5 +8,6 @@ public static class SettingsProvider
     public static IConfiguration Initialize() => 
         new ConfigurationBuilder()
             .AddJsonFile(Path.Join("Settings", "appsettings.json"))
+            .AddJsonFile(Path.Join("Settings", "appsettings.local.json"), true)
             .Build();
 }

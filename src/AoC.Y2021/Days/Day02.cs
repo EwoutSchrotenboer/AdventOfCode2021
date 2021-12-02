677a8c0507ddd3467f02d1ca63cb6b117c7b3237
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-02 06:43:31 +0100

    Day 2: Dive!

--- src/AoC.Y2021/Days/Day02.cs
+++ src/AoC.Y2021/Days/Day02.cs
@@ -1,4 +1,6 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,40 @@ public class Day02 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => FollowCourse(_puzzleInput.GetLines().Select(GetInstruction), false);
 
-    public override IComparable PartTwo() => -1;
+    public override IComparable PartTwo() => FollowCourse(_puzzleInput.GetLines().Select(GetInstruction), true);
+
+    private int FollowCourse(IEnumerable<(string, int)> instructions, bool useAim)
+    {
+        var horizontal = 0;
+        var depth = 0;
+        var aim = 0;
+
+        foreach (var (direction, value) in instructions)
+        {
+            if (direction.Equals("vertical"))
+            {
+                if (useAim) { aim += value; }
+                else { depth += value; }
+            }
+            else
+            {
+                horizontal += value;
+                if (useAim) { depth += aim * value; }
+            }
+        }
+
+        return horizontal * depth;
+    }
+
+    private (string direction, int value) GetInstruction(string line)
+    {
+        var items = line.Split(' ');
+        var direction = items[0];
+        var value = int.Parse(items[1]);
+
+        if (direction.Equals("forward")) { return (direction, value); }       
+        return ("vertical", direction.Equals("down") ? value : -value);
+    }
 }
--- tests/AoC.Y2021.Tests/Days/Day02Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day02Tests.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AoC.Common;
 using AoC.Y2021.Days;
@@ -9,14 +9,14 @@ namespace AoC.Y2021.Tests.Days;
 public class Day02Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string>() { "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day02(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(150, actual);
     }
 
     [Fact]
@@ -25,7 +25,7 @@ public class Day02Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 02);
         var target = new Day02(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(1499229, actual);
     }
 
     [Fact]
@@ -33,7 +33,7 @@ public class Day02Tests
     {
         var target = new Day02(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(900, actual);
     }
 
     [Fact]
@@ -42,6 +42,6 @@ public class Day02Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 02);
         var target = new Day02(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(1340836560, actual);
     }
 }

8bce3987442e78d2af9b429e69f3c96f3a043acf
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-04 06:59:02 +0100

    Day 4: Giant Squid

--- src/AoC.Y2021/Days/Day04.cs
+++ src/AoC.Y2021/Days/Day04.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,77 @@ public class Day04 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => PlayBingo(_puzzleInput.GetGroups(), true);
 
-    public override IComparable PartTwo() => -1;
+    public override IComparable PartTwo() => PlayBingo(_puzzleInput.GetGroups(), false);
+    private int PlayBingo(List<List<string>> inputGroups, bool firstBingo)
+    {
+        var numbers = inputGroups.First()[0].Split(',').Select(int.Parse);
+        var cards = inputGroups.Skip(1).Select(g => new Bingoboard(g)).ToList();
+
+        foreach (var number in numbers)
+        {
+            foreach (var card in cards)
+            {
+                var (bingo, sumOfUnmarked) = card.AddNumber(number);
+
+                if (bingo && (firstBingo || cards.All(c => c.Complete)))
+                {
+                    return sumOfUnmarked * number;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+public class Bingoboard
+{
+    public bool Complete { get; private set; }
+    private readonly Dictionary<int, Position> _spaces = new();
+    private readonly Dictionary<int, Position> _called = new();
+
+    public Bingoboard(List<string> input)
+    {
+        for (int y = 0; y < input.Count; y++)
+        {
+            var rowItems = input[y].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int x = 0; x < rowItems.Length; x++)
+            {
+                _spaces.Add(int.Parse(rowItems[x]), new Position(x, y));
+            }
+        }
+    }
+
+    public (bool bingo, int sumOfUnmarked) AddNumber(int number)
+    {
+        if (_spaces.ContainsKey(number))
+        {
+            _called.Add(number, _spaces[number]);
+        }
+
+        return CheckBingo();
+    }
+
+    private (bool bingo, int sumOfUnmarked) CheckBingo()
+    {
+        if (_called.Keys.Count < 5) { return (false, -1); }
+
+        var calledSpaces = _called.Values;
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (calledSpaces.Count(v => v.X == i) == 5 || calledSpaces.Count(v => v.Y == i) == 5)
+            {
+                Complete = true;
+                return (true, GetSumOfUncalled());
+            }
+        }
+
+        return (false, -1);
+    }
+
+    private int GetSumOfUncalled() => _spaces.Keys.Except(_called.Keys).Sum();
 }
--- tests/AoC.Y2021.Tests/Days/Day04Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day04Tests.cs
@@ -9,14 +9,23 @@ namespace AoC.Y2021.Tests.Days;
 public class Day04Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string>
+    {
+        "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+        "",
+        "22 13 17 11  0", " 8  2 23  4 24", "21  9 14 16  7", " 6 10  3 18  5", " 1 12 20 15 19",
+        "",
+        " 3 15  0  2 22", " 9 18 13 17  5", "19  8  7 25 23", "20 11 10 24  4", "14 21 16 12  6",
+        "",
+        "14 21 17 24  4", "10 16 15  9 19", "18  8 23 26 20", "22 11 13  6  5", " 2  0 12  3  7"
+    });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day04(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(4512, actual);
     }
 
     [Fact]
@@ -25,7 +34,7 @@ public class Day04Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 04);
         var target = new Day04(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(8580, actual);
     }
 
     [Fact]
@@ -33,7 +42,7 @@ public class Day04Tests
     {
         var target = new Day04(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(1924, actual);
     }
 
     [Fact]
@@ -42,6 +51,6 @@ public class Day04Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 04);
         var target = new Day04(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(9576, actual);
     }
 }

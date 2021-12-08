8baf31d1978d604359fddb80beefb69a45f69beb
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-08 08:05:31 +0100

    Day 8: Seven Segment Search

--- src/AoC.Y2021/Days/Day08.cs
+++ src/AoC.Y2021/Days/Day08.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,76 @@ public class Day08 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => GetEntries().Select(o => o.outputs).Sum(o => o.Count(s => MapUniqueSignals(s) != -1));
 
-    public override IComparable PartTwo() => -1;
+    public override IComparable PartTwo()
+    {
+        var entries = GetEntries();
+        var outputValues = new List<string>();
+
+        foreach (var (signals, outputs) in entries)
+        {
+            var mappedSignals = new string[10];
+
+            foreach (var signal in signals)
+            {
+                var mapped = MapUniqueSignals(signal);
+                if (mapped != -1) { mappedSignals[mapped] = signal; }
+            }
+
+            var fives = signals.Where(s => s.Length == 5).ToList();
+            var sixes = signals.Where(s => s.Length == 6).ToList();
+
+            mappedSignals[6] = GetSix(sixes, mappedSignals[1][0], mappedSignals[1][1]);
+            var lowerRight = mappedSignals[6].Single(s => mappedSignals[1].Contains(s));
+            var upperRight = mappedSignals[1].Single(s => s != lowerRight);
+
+            mappedSignals[5] = fives.Single(s => !s.Contains(upperRight));
+            var lowerLeft = mappedSignals[6].Single(s => !mappedSignals[5].Contains(s));
+
+            mappedSignals[2] = fives.Single(s => !s.Contains(lowerRight));
+            mappedSignals[3] = fives.Single(s => s != mappedSignals[2] && s != mappedSignals[5]);
+            mappedSignals[9] = sixes.Single(s => !s.Contains(lowerLeft));
+            mappedSignals[0] = sixes.Single(s => s != mappedSignals[9] && s != mappedSignals[6]);
+
+            var orderedSignals = mappedSignals.Select(Order).ToList();
+
+            var number = outputs.Aggregate(string.Empty, (current, output) => current + orderedSignals.IndexOf(Order(output)));
+            outputValues.Add(number);
+        }
+
+        return outputValues.Sum(int.Parse);
+    }
+
+    private string Order(string value) => string.Concat(value.OrderBy(c => c));
+
+    private int MapUniqueSignals(string input) =>
+        input.Length switch
+        {
+            2 => 1,
+            4 => 4,
+            3 => 7,
+            7 => 8,
+            _ => -1
+        };
+
+    private string GetSix(IEnumerable<string> signals, char a, char b) =>
+        signals.Single(s => s.Length == 6 && (s.Contains(a) && !s.Contains(b) || !s.Contains(a) && s.Contains(b)));
+
+    private List<(List<string> signals, List<string> outputs)> GetEntries()
+    {
+        var lines = _puzzleInput.GetLines();
+        var entries = new List<(List<string>, List<string>)>();
+
+        foreach (var line in lines)
+        {
+            var items = line.Split(" | ");
+            var signals = items[0].Split(' ').ToList();
+            var outputs = items[1].Split(' ').ToList();
+
+            entries.Add((signals, outputs));
+        }
+
+        return entries;
+    }
 }
--- tests/AoC.Y2021.Tests/Days/Day08Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day08Tests.cs
@@ -9,14 +9,26 @@ namespace AoC.Y2021.Tests.Days;
 public class Day08Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string>
+    {
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+    });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day08(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(26, actual);
     }
 
     [Fact]
@@ -25,7 +37,7 @@ public class Day08Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 08);
         var target = new Day08(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(288, actual);
     }
 
     [Fact]
@@ -33,7 +45,7 @@ public class Day08Tests
     {
         var target = new Day08(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(61229, actual);
     }
 
     [Fact]
@@ -42,6 +54,6 @@ public class Day08Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 08);
         var target = new Day08(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(940724, actual);
     }
 }

595922fb7713009e546856724561a8373db03f4f
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-11 07:51:38 +0100

    Day 11: Dumbo Octopus

--- src/AoC.Y2021/Days/Day11.cs
+++ src/AoC.Y2021/Days/Day11.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,92 @@ public class Day11 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => Simulate(false, 100).flashes;
 
-    public override IComparable PartTwo() => -1;
+    public override IComparable PartTwo() => Simulate(true).step;
+
+    private (int step, int flashes) Simulate(bool indefinite, int steps = 0)
+    {
+        var grid = GetOctopusGrid();
+        var step = 0;
+        var flashes = 0;
+        while (indefinite || step < steps)
+        {
+            step++;
+            flashes += Step(grid);
+
+            if (grid.Values.All(v => v == 0))
+            {
+                return (step, flashes);
+            }
+        }
+
+        return (step, flashes);
+    }
+
+    private int Step(Dictionary<(int, int), int> grid)
+    {
+        var flashes = 0;
+        foreach (var pos in grid.Keys) { grid[pos]++; }
+
+        var flashed = new HashSet<(int, int)>();
+        var toFlash = new Queue<(int, int)>(grid.Where(o => o.Value > 9).Select(o => o.Key));
+
+        while (toFlash.Count > 0)
+        {
+            var current = toFlash.Dequeue();
+            if (!flashed.Add(current)) { continue; }
+
+            flashes++;
+            var neighbors = GetNeighbours(current).Where(grid.ContainsKey);
+
+            foreach (var n in neighbors)
+            {
+                grid[n]++;
+
+                if (grid[n] > 9 && !flashed.Contains(n))
+                {
+                    toFlash.Enqueue(n);
+                }
+            }
+        }
+
+        foreach (var f in flashed)
+        {
+            grid[f] = 0;
+        }
+
+        return flashes;
+    }
+
+    private Dictionary<(int, int), int> GetOctopusGrid()
+    {
+        var lines = _puzzleInput.GetLines();
+        var grid = new Dictionary<(int, int), int>();
+
+        for (int y = 0; y < lines.Count; y++)
+        {
+            for (int x = 0; x < lines[0].Length; x++)
+            {
+                grid.Add((x, y), int.Parse($"{lines[y][x]}"));
+            }
+        }
+
+        return grid;
+    }
+
+    private IEnumerable<(int, int)> GetNeighbours((int, int) pos)
+    {
+        var (x, y) = pos;
+        yield return (x - 1, y - 1);
+        yield return (x - 1, y);
+        yield return (x - 1, y + 1);
+
+        yield return (x, y - 1);
+        yield return (x, y + 1);
+
+        yield return (x + 1, y - 1);
+        yield return (x + 1, y);
+        yield return (x + 1, y + 1);
+    }
 }
--- tests/AoC.Y2021.Tests/Days/Day11Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day11Tests.cs
@@ -9,14 +9,14 @@ namespace AoC.Y2021.Tests.Days;
 public class Day11Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string> { "5483143223", "2745854711", "5264556173", "6141336146", "6357385478", "4167524645", "2176841721", "6882881134", "4846848554", "5283751526" });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day11(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(1656, actual);
     }
 
     [Fact]
@@ -25,7 +25,7 @@ public class Day11Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 11);
         var target = new Day11(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(1665, actual);
     }
 
     [Fact]
@@ -33,7 +33,7 @@ public class Day11Tests
     {
         var target = new Day11(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(195, actual);
     }
 
     [Fact]
@@ -42,6 +42,6 @@ public class Day11Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 11);
         var target = new Day11(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(235, actual);
     }
 }

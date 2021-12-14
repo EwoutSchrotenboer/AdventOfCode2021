68925cb6d6e75884e5661cacb9ce1d67b5fc86fd
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-14 07:15:39 +0100

    Day 14: Extended Polymerization

--- src/AoC.Y2021/Days/Day14.cs
+++ src/AoC.Y2021/Days/Day14.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,82 @@ public class Day14 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => CalculatePolymer(10);
 
-    public override IComparable PartTwo() => -1;
+    public override IComparable PartTwo() => CalculatePolymer(40);
+
+    public long CalculatePolymer(int iterations)
+    {
+        var (initial, rules) = GetPolymerRules();
+        var occurrences = new Dictionary<string, long>();
+
+        for (int index = 0; index < initial.Length - 1; index++)
+        {
+            var v = initial.Substring(index, 2);
+            if (!occurrences.ContainsKey(v)) { occurrences.Add(v, 0);}
+            occurrences[v]++;
+        }
+
+        for (int i = 0; i < iterations; i++)
+        {
+            occurrences = Step(occurrences, rules);
+        }
+
+        var totals = GetTotals(occurrences, initial[^1]);
+        return totals.Values.Max() - totals.Values.Min();
+    }
+
+    private Dictionary<string, long> Step(Dictionary<string, long> occurrences, Dictionary<string, string> rules)
+    {
+        var next = new Dictionary<string, long>();
+
+        foreach (var (key, value) in occurrences)
+        {
+            var first = $"{key[0]}{rules[key]}";
+            var second = $"{rules[key]}{key[1]}";
+
+            if (!next.ContainsKey(first)) { next.Add(first, 0); }
+
+            next[first] += value;
+
+            if (!next.ContainsKey(second))
+            {
+                next.Add(second, 0);
+            }
+
+            next[second] += value;
+        }
+
+        return next;
+    }
+
+    private Dictionary<char, long> GetTotals(Dictionary<string, long> occurrences, char last)
+    {
+        var totals = new Dictionary<char, long>();
+
+        foreach (var (key, value) in occurrences)
+        {
+            if (!totals.ContainsKey(key[0])) { totals.Add(key[0], 0); }
+            totals[key[0]] += value;
+        }
+
+        totals[last]++;
+
+        return totals;
+    }
+
+    private (string initial, Dictionary<string, string> rules) GetPolymerRules()
+    {
+        var lines = _puzzleInput.GetLines();
+
+        var initial = lines.First();
+        var rules = new Dictionary<string, string>();
+        foreach (var rule in lines.Skip(2))
+        {
+            var items = rule.Split(" -> ");
+            rules.Add(items[0], items[1]);
+        }
+
+        return (initial, rules);
+    }
 }
--- tests/AoC.Y2021.Tests/Days/Day14Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day14Tests.cs
@@ -9,14 +9,14 @@ namespace AoC.Y2021.Tests.Days;
 public class Day14Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string> { "NNCB", "", "CH -> B", "HH -> N", "CB -> H", "NH -> C", "HB -> C", "HC -> B", "HN -> C", "NN -> C", "BH -> H", "NC -> B", "NB -> B", "BN -> B", "BB -> N", "BC -> B", "CC -> N", "CN -> C", });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day14(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(1588L, actual);
     }
 
     [Fact]
@@ -25,7 +25,7 @@ public class Day14Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 14);
         var target = new Day14(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(3406L, actual);
     }
 
     [Fact]
@@ -33,7 +33,7 @@ public class Day14Tests
     {
         var target = new Day14(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(2188189693529, actual);
     }
 
     [Fact]
@@ -42,6 +42,6 @@ public class Day14Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 14);
         var target = new Day14(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(3941782230241, actual);
     }
 }

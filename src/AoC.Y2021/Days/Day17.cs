2ca2399e702de8288c182dd572fc3dc0a20b071c
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-17 07:21:49 +0100

    Day 17: Trick Shot

--- src/AoC.Y2021/Days/Day17.cs
+++ src/AoC.Y2021/Days/Day17.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
+using AoC.Common.Extensions;
+using AoC.Common.Vectors;
 
 namespace AoC.Y2021.Days;
 
@@ -9,7 +13,57 @@ public class Day17 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => GetValidTrajectories().maxHeight;
+    public override IComparable PartTwo() => GetValidTrajectories().count;
 
-    public override IComparable PartTwo() => -1;
+    private (int maxHeight, int count) GetValidTrajectories()
+    {
+        var (xMin, xMax, yMin, yMax) = GetTargetArea();
+        var trajectories = new List<int>();
+
+        for (int yi = -250; yi < 250; yi++)
+        {
+            for (int xi = 1; xi < 100; xi++)
+            {
+                var (vX, vY) = (xi, yi);
+                var (x, y) = (0, 0);
+
+                var top = 0;
+                var hitTarget = false;
+                while (x <= xMax && y >= yMin)
+                {
+                    x += vX;
+                    y += vY;
+
+                    if (y > top) { top = y; }
+
+                    if (vX > 0) { vX--; }
+                    else if (vX < 0) { vX++; }
+                    vY--;
+
+                    if (x >= xMin && x <= xMax && y >= yMin && y <= yMax)
+                    {
+                        hitTarget = true;
+                    }
+                }
+
+                if (hitTarget)
+                {
+                    trajectories.Add(top);
+                }
+            }
+        }
+
+        return (trajectories.Max(), trajectories.Count);
+    }
+
+    private (int xMin, int xMax, int yMin, int yMax) GetTargetArea()
+    {
+        var items = _puzzleInput.GetLine()
+            .Split(new[] { "x=", "..", "y=", "target area: ", " ", "," }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        return (items[0], items[1], items[2], items[3]);
+    }
 }
--- tests/AoC.Y2021.Tests/Days/Day17Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day17Tests.cs
@@ -9,14 +9,14 @@ namespace AoC.Y2021.Tests.Days;
 public class Day17Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string> { "target area: x=20..30, y=-10..-5" });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day17(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(45, actual);
     }
 
     [Fact]
@@ -25,7 +25,7 @@ public class Day17Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 17);
         var target = new Day17(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(23005, actual);
     }
 
     [Fact]
@@ -33,7 +33,7 @@ public class Day17Tests
     {
         var target = new Day17(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(112, actual);
     }
 
     [Fact]
@@ -42,6 +42,6 @@ public class Day17Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 17);
         var target = new Day17(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(2040, actual);
     }
 }

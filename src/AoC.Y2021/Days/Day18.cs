647d6d10e207e5f26e8cd705597f8a6942680b32
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-18 11:12:36 +0100

    Day 18: Snailfish

--- src/AoC.Y2021/Days/Day18.cs
+++ src/AoC.Y2021/Days/Day18.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +12,144 @@ public class Day18 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne()
+    {
+        var snailNumbers = _puzzleInput.GetLines().Select(ReadSnail).ToArray();
+        var snailNumber = snailNumbers[0];
+        snailNumber.Reduce();
 
-    public override IComparable PartTwo() => -1;
+        for (int i = 1; i < snailNumbers.Length; i++)
+        {
+            snailNumbers[i].Reduce();
+            snailNumber = snailNumber.Add(snailNumbers[i]);
+            snailNumber.Reduce();
+        }
+
+        return snailNumber.Magnitude;
+    }
+
+    public override IComparable PartTwo()
+    {
+        var lines = _puzzleInput.GetLines();
+        var max = 0;
+        for (int firstIndex = 0; firstIndex < lines.Count; firstIndex++)
+        {
+            for (int secondIndex = 0; secondIndex < lines.Count; secondIndex++)
+            {
+                if (firstIndex == secondIndex) { continue; }
+
+                var first = ReadSnail(lines[firstIndex]);
+                var second = ReadSnail(lines[secondIndex]);
+                first.Reduce();
+                second.Reduce();
+
+                var sum = first.Add(second);
+                sum.Reduce();
+                var magnitude = sum.Magnitude;
+                if (magnitude > max) { max = magnitude; }
+            }
+        }
+
+        return max;
+    }
+
+    private SnailNumber ReadSnail(string number) => ReadSnail(number, 0, 0).snail;
+
+    private (SnailNumber snail, int pos) ReadSnail(string number, int start, int depth)
+    {
+        if (number[start] == '[')
+        {
+            var (left, posRight) = ReadSnail(number, start + 1, depth + 1);
+            var (right, posLeft) = ReadSnail(number, posRight + 1, depth + 1);
+            var snail = new SnailNumber { Left = left, Right = right, Depth = depth };
+            left!.Parent = snail;
+            right!.Parent = snail;
+            return (snail, posLeft + 1);
+        }
+
+        var value = 0;
+        var pos = start;
+        while (number[pos] >= '0' && number[pos] <= '9')
+        {
+            value = value * 10 + (number[pos] - '0');
+            pos++;
+        }
+        return (new SnailNumber { Value = value, Depth = depth }, pos);
+    }
+
+    private class SnailNumber : IEnumerable<SnailNumber>
+    {
+        public int? Value { get; set; }
+        public SnailNumber? Left { get; set; }
+        public SnailNumber? Right { get; set; }
+        public SnailNumber? Parent { get; set; }
+        public int Depth { get; set; }
+        public int Magnitude => Value ?? Left!.Magnitude * 3 + Right!.Magnitude * 2;
+        private bool IsPair => !Value.HasValue && Left?.Value is not null && Right?.Value is not null;
+        private SnailNumber Top => Parent is null ? this : Parent.Top;
+
+        public void Reduce()
+        {
+            while (true)
+            {
+                var toExplode = this.FirstOrDefault(s => s.Depth == 4 && s.IsPair);
+                if (toExplode is not null) { toExplode.Explode(); continue; }
+
+                var toSplit = this.FirstOrDefault(s => s.Value > 9);
+                if (toSplit is not null) { toSplit.Split(); continue; }
+                break;
+            }
+        }
+
+        public SnailNumber Add(SnailNumber snailNumber)
+        {
+            var depth = Depth;
+            foreach (var s in this) { s.Depth++; }
+            foreach (var s in snailNumber) { s.Depth++; }
+
+            var parent = new SnailNumber
+            {
+                Left = this,
+                Right = snailNumber,
+                Depth = depth,
+            };
+            Parent = parent;
+            snailNumber.Parent = parent;
+            return parent;
+        }
+
+        private void Explode()
+        {
+            var previous = Top.Where(x => x.Value.HasValue).TakeWhile(x => x != this.Left).LastOrDefault();
+            if (previous is not null) { previous.Value += Left?.Value; }
+
+            var next = Top.Where(x => x.Value.HasValue).SkipWhile(x => x != this.Right).Skip(1).FirstOrDefault();
+            if (next is not null) { next.Value += Right?.Value; }
+
+            Left = null;
+            Right = null;
+            Value = 0;
+        }
+
+        private void Split()
+        {
+            if (Value is null) { return; }
+            Left = new SnailNumber { Value = (int)Math.Floor((double)Value / 2), Depth = Depth + 1, Parent = this };
+            Right = new SnailNumber { Value = (int)Math.Ceiling((double)Value / 2), Depth = Depth + 1, Parent = this };
+            Value = null;
+        }
+
+        public IEnumerator<SnailNumber> GetEnumerator()
+        {
+            yield return this;
+            if (Value.HasValue) { yield break; }
+            foreach (var snail in Left ?? Enumerable.Empty<SnailNumber>()) { yield return snail; }
+            foreach (var snail in Right ?? Enumerable.Empty<SnailNumber>()) { yield return snail; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 }
--- tests/AoC.Y2021.Tests/Days/Day18Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day18Tests.cs
@@ -9,14 +9,14 @@ namespace AoC.Y2021.Tests.Days;
 public class Day18Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string> { "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]", "[[[5,[2,8]],4],[5,[[9,9],0]]]", "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]", "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]", "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]", "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]", "[[[[5,4],[7,7]],8],[[8,3],8]]", "[[9,3],[[9,9],[6,[4,9]]]]", "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]", "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]", });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day18(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(4140, actual);
     }
 
     [Fact]
@@ -25,7 +25,7 @@ public class Day18Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 18);
         var target = new Day18(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(3892, actual);
     }
 
     [Fact]
@@ -33,7 +33,7 @@ public class Day18Tests
     {
         var target = new Day18(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(3993, actual);
     }
 
     [Fact]
@@ -42,6 +42,6 @@ public class Day18Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 18);
         var target = new Day18(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(4909, actual);
     }
 }

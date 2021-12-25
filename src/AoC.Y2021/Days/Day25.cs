6ce0cc0df2368cb8cb6d8c6804e48b504035e05a
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-25 09:03:20 +0100

    Day 25: Sea Cucumber

--- src/AoC.Y2021/Days/Day25.cs
+++ src/AoC.Y2021/Days/Day25.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,45 @@ public class Day25 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne()
+    {
+        var cucumberTypes = new List<(char c, (int x, int y))> { ('>', (0, 1)), ('v', (1, 0)) };
+        var patch = GetCucumberPatch();
+
+        var yMax = patch.Length;
+        var xMax = patch[0].Length;
+
+        for (int step = 1; step < int.MaxValue; step++)
+        {
+            var moved = false;
+            foreach (var (c, (yv, xv)) in cucumberTypes)
+            {
+                var next = ClonePatch(patch);
+                for (int y = 0; y < yMax; y++)
+                {
+                    for (int x = 0; x < xMax; x++)
+                    {
+                        var (ya, xa) = ((y + yv) % yMax, (x + xv) % xMax);
+                        if (patch[y][x] == c && patch[ya][xa] == '.')
+                        {
+                            next[ya][xa] = c;
+                            next[y][x] = '.';
+                            moved = true;
+                        }
+                    }
+                }
+
+                patch = next;
+            }
+
+            if (!moved) { return step; }
+        }
+
+        return -1;
+    }
+
+    private char[][] ClonePatch(char[][] patch) => patch.Select(l => l.ToArray()).ToArray();
+    private char[][] GetCucumberPatch() => _puzzleInput.GetLines().Select(l => l.ToArray()).ToArray();
 
     public override IComparable PartTwo() => -1;
 }
--- tests/AoC.Y2021.Tests/Days/Day25Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day25Tests.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AoC.Common;
 using AoC.Y2021.Days;
@@ -9,14 +9,14 @@ namespace AoC.Y2021.Tests.Days;
 public class Day25Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string> { "v...>>.vv>", ".vv>>.vv..", ">>.>v>...v", ">>v>>.>.v.", "v>v.vv.v..", ">.>>..v...", ".vv..>.>v.", "v.v..>>v.v", "....v..v.>" });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day25(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(58, actual);
     }
 
     [Fact]
@@ -25,6 +25,6 @@ public class Day25Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 25);
         var target = new Day25(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(458, actual);
     }
 }

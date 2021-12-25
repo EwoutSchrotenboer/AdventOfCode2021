4deabe0cb333f555c0e1f44fc596238694c7b569
Ewout Schrotenboer <ewout@schrotenboer.nl>
2021-12-25 09:02:57 +0100

    Day 24: Arithmetic Logic Unit

--- src/AoC.Y2021/Days/Day24.cs
+++ src/AoC.Y2021/Days/Day24.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 
 namespace AoC.Y2021.Days;
@@ -9,7 +11,54 @@ public class Day24 : DayBase
     {
     }
 
-    public override IComparable PartOne() => -1;
+    public override IComparable PartOne() => TransformToSerial(RunSerials(true));
 
-    public override IComparable PartTwo() => -1;
+    public override IComparable PartTwo() => TransformToSerial(RunSerials(false));
+
+    private Dictionary<int, int> RunSerials(bool getMax)
+    {
+        var pairs = GetPairs();
+        var stack = new Stack<(int, int)>();
+        var keys = new Dictionary<int, (int x, int y)>();
+
+        foreach (var ((n, m), i) in pairs.Select((pair, i) => (pair, i)))
+        {
+            if (n > 0)
+            {
+                stack.Push((i, m));
+            }
+            else
+            {
+                var (j, address) = stack.Pop();
+                keys[i] = (j, address + n);
+            }
+        }
+
+        return GenerateOutput(keys, getMax);
+    }
+
+    private Dictionary<int, int> GenerateOutput(Dictionary<int, (int x, int y)> keys, bool getMax)
+    {
+        var output = new Dictionary<int, int>();
+
+        foreach (var (key, (x, y)) in keys)
+        {
+            output[key] = getMax ? Math.Min(9, 9 + y) : Math.Max(1, 1 + y);
+            output[x] = getMax ? Math.Min(9, 9 - y) : Math.Max(1, 1 - y);
+        }
+
+        return output;
+    }
+
+    private List<(int, int)> GetPairs()
+    {
+        var lines = _puzzleInput.GetLines();
+        return Enumerable.Range(0, 14)
+            .Select(i => (
+                int.Parse(lines[i * 18 + 5][6..]),
+                int.Parse(lines[i * 18 + 15][6..])))
+            .ToList();
+    }
+
+    private long TransformToSerial(Dictionary<int, int> digits) => long.Parse(string.Join("", digits.OrderBy(x => x.Key).Select(x => x.Value)));
 }
--- tests/AoC.Y2021.Tests/Days/Day24Tests.cs
+++ tests/AoC.Y2021.Tests/Days/Day24Tests.cs
@@ -9,14 +9,37 @@ namespace AoC.Y2021.Tests.Days;
 public class Day24Tests
 {
     private readonly PuzzleInputProvider _puzzleInputProvider = new();
-    private readonly PuzzleInput _testInput = new(new List<string>());
+    private readonly PuzzleInput _testInput = new(new List<string>
+        // Each position is determined by running 18 operations. Later operations are influenced by earlier steps
+        // W always is $input, X starts each loop as 0, Y starts each loop as 0, Z carries over
+        // X = Z % 26
+        // Z /= 1 or 26
+        // X += $value
+        // X = (X != $input) ? 1 : 0
+        // Y = (25 * X) + 1
+        // Z *= Y
+        // Z += ($input + $value) * X
+      { "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 12",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 4",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 11",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 11", "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 13",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 5",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 11",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 11", "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 14",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 14", "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -10", "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 7",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 11",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 11", "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -9",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 4",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -3",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 6",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 1",  "add x 13",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 5",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -5",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 9",  "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -10", "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 12", "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -4",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 14", "mul y x", "add z y",
+        "inp w", "mul x 0", "add x z", "mod x 26", "div z 26", "add x -5",  "eql x w", "eql x 0", "mul y 0", "add y 25", "mul y x", "add y 1", "mul z y", "mul y 0", "add y w", "add y 14", "mul y x", "add z y" });
 
     [Fact]
     public void PartOneTest()
     {
         var target = new Day24(_testInput);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(92915979999498, actual);
     }
 
     [Fact]
@@ -25,7 +48,7 @@ public class Day24Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 24);
         var target = new Day24(input);
         var actual = target.PartOne();
-        Assert.Equal(-1, actual);
+        Assert.Equal(92915979999498, actual);
     }
 
     [Fact]
@@ -33,7 +56,7 @@ public class Day24Tests
     {
         var target = new Day24(_testInput);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(21611513911181, actual);
     }
 
     [Fact]
@@ -42,6 +65,6 @@ public class Day24Tests
         var input = await _puzzleInputProvider.GetAsync(2021, 24);
         var target = new Day24(input);
         var actual = target.PartTwo();
-        Assert.Equal(-1, actual);
+        Assert.Equal(21611513911181, actual);
     }
 }
